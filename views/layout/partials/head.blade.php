4b724416b64abf12c709d3d94eef07be11d402c5
Andrew Schmelyun <schmelyun@gmail.com>
2017-12-08 02:48:24 -0500

    Manifest inlined for best practices

--- views/layout/partials/head.blade.php
+++ views/layout/partials/head.blade.php
@@ -4,3 +4,4 @@
 <title>{{ $title or 'Andrew Schmelyun - Full Stack Developer On The Edge' }}</title>
 <link rel="stylesheet" type="text/css" href="{{ str_replace('/dist/', '', $mix_manifest['/dist/assets/css/app.css']) }}">
 <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700|Source+Serif+Pro" rel="stylesheet"> 
+<link rel="manifest" href='data:application/manifest+json, {"name": "ASchmelyun", "short_name": "ASchmelyun"}'>
